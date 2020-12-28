ea3e7ec05ce16137fc4451232582c7b4392b45cd
Daniel Kunkler <kunkl007@umn.edu>
2020-12-28 14:04:21 -0600

    Changed it so space bar pauses/unpauses

--- main.cpp
+++ main.cpp
@@ -5,47 +5,65 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Conways Game of Life");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Conways Game of Life");
 
-  GameBoard board;
+    GameBoard board;
 
-  while (window.isOpen())
-  {
-    // event loop
-    sf::Event event;
-    while (window.pollEvent(event))
+    bool sim_running = false;
+
+    while (window.isOpen())
     {
-      if (event.type == sf::Event::Closed)
-        window.close();
-
-      else if (event.type == sf::Event::MouseButtonPressed)
-      {
-        auto pos = sf::Mouse::getPosition(window);
-        int x = pos.y;
-        int y = pos.x;
-        board.click(x, y);
-      }
-
-      else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-      {
-        board.iterate();
-      }
-
-      else if (event.type == sf::Event::KeyPressed)
-      {
-        if (event.key.code == sf::Keyboard::BackSpace)
+        // event loop
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-          board.reset();
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                auto pos = sf::Mouse::getPosition(window);
+                int x = pos.y;
+                int y = pos.x;
+                board.click(x, y);
+            }
+
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Space) {
+                    sim_running = !sim_running;
+                    //Test
+                    //sim_running = true;
+                }
+                else if (event.key.code == sf::Keyboard::BackSpace) {
+                    board.reset();
+                    sim_running = false;
+                }
+            }
+            //else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+            //{
+            //    sim_running = true;
+            //  //board.iterate();
+            //}
+
+            //else if (event.type == sf::Event::KeyPressed)
+            //{
+            //  if (event.key.code == sf::Keyboard::BackSpace)
+            //  {
+            //    board.reset();
+            //    sim_running = false;
+            //  }
+            //} //Hello
         }
-      }
+        if (sim_running) {
+            board.iterate();
+        }
+
+        window.clear();
+
+        board.draw(&window);
+
+        window.display();
     }
 
-    window.clear();
-
-    board.draw(&window);
-
-    window.display();
-  }
-
-  return 0;
+    return 0;
 }
